a079446a955ad04210c02093c58c09252121a9ff
Wallace Gonçalves <wallacecosme@gmail.com>
2021-08-07 20:41:11 -0300

    Config client

--- book-server/src/main/java/br/com/instalar/bookservice/BookServerApplication.java
+++ book-server/src/main/java/br/com/instalar/bookservice/BookServerApplication.java
@@ -2,7 +2,9 @@ package br.com.instalar.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class BookServerApplication {
 
