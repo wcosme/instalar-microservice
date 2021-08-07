581e5efd7c382ceee7eb4d2741c370f3c659c141
Wallace Gonçalves <wallacecosme@gmail.com>
2021-08-07 11:50:13 -0300

    Implementando o database

--- cambio-service/src/main/java/br/com/instalar/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/instalar/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.instalar.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,9 @@ import br.com.instalar.model.Cambio;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+	
+	@Autowired
+	private Environment environment;
 
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
@@ -19,6 +24,8 @@ public class CambioController {
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
 		
-		return new Cambio(10L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		String port = environment.getProperty("local.server.port");
+		
+		return new Cambio(5L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
