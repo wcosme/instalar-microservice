4b3b7ad3bd153268eac50813f2549fe880ca4763
Wallace Gonçalves <wallacecosme@gmail.com>
2021-08-07 10:41:08 -0300

    Refatoramento do projeto

--- cambio-service/src/test/java/br/com/instalar/CambioServiceApplicationTests.java
+++ cambio-service/src/test/java/br/com/instalar/CambioServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.instalar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- config-server/src/main/java/br/com/instalar/ConfigServerApplication.java
+++ config-server/src/main/java/br/com/instalar/ConfigServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
-public class InstalarConfigServerApplication {
+@SpringBootApplication
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InstalarConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
