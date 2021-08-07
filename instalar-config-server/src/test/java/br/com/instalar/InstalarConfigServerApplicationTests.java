7ef4215a81c34514d155e34c6716b65add7cbdfc
Wallace Gonçalves <wallacecosme@gmail.com>
2021-08-06 22:24:44 -0300

    Criação do projeto

---
+++ instalar-config-server/src/test/java/br/com/instalar/InstalarConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.instalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstalarConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
