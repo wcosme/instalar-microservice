4c71b0376474fd1ec00a50ad31c546c1c0f73cd5
Wallace Gonçalves <wallacecosme@gmail.com>
2021-08-09 22:30:46 -0300

    Implementando loadBalance

--- book-server/src/main/java/br/com/instalar/bookservice/controller/BookController.java
+++ book-server/src/main/java/br/com/instalar/bookservice/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.instalar.bookservice.model.Book;
+import br.com.instalar.bookservice.proxy.CambioProxy;
 import br.com.instalar.bookservice.repository.BookRepository;
 
 @RestController
@@ -18,20 +19,25 @@ public class BookController {
 	private BookRepository repository;	
 	@Autowired
 	private Environment environment;
+	@Autowired
+	private CambioProxy proxy;
 
-	@GetMapping(value = "/{id}/{currency}")
+	@GetMapping(value = "/{id}/{currency}")	
 	public Book findBook(
-			@PathVariable ("id") Long id,
-			@PathVariable ("currency") String currency) {
+			@PathVariable("id") Long id,
+			@PathVariable("currency") String currency
+			) {
 		
 		var book = repository.getById(id);
-		if(book == null) {
-			throw new RuntimeException("Book not found");
-		}
+		if (book == null) throw new RuntimeException("Book not Found");
+				
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 		
 		var port = environment.getProperty("local.server.port");
-		book.setEnvironment(port);		
-		
+		book.setEnvironment(
+				"Book port: " + port + 
+				" Cambio Port " + cambio.getEnvironment());
+		book.setPrice(cambio.getConvertedValue());
 		return book;
 	}
 }
---
+++ book-server/src/main/java/br/com/instalar/bookservice/model/Cambio.java
@@ -0,0 +1,30 @@
+package br.com.instalar.bookservice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Cambio implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Long id;	
+	private String from;	
+	private String to;
+	private BigDecimal conversionFactor;
+	private Double convertedValue;
+	private String environment;
+		
+
+}
